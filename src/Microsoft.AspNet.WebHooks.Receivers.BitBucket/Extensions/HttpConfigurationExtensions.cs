5c480beb9f12a58321ee076e101ce43f35423755
Gaurav Arora <g_arora@hotmail.com>
2015-09-13 03:45:15 +0530

    This is not running. Its just a hypothetical idea to create ASP.NET WebHook for BitBucket, these are slightly different from GitHub, need corrections

---
+++ src/Microsoft.AspNet.WebHooks.Receivers.BitBucket/Extensions/HttpConfigurationExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+using Microsoft.AspNet.WebHooks.Config;
+
+namespace System.Web.Http
+{
+    /// <summary>
+    /// Extension methods for <see cref="HttpConfiguration"/>.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class HttpConfigurationExtensions
+    {
+        /// <summary>
+        /// Initializes support for receiving BitBucket WebHooks.
+        /// Set the '<c>MS_WebHookReceiverSecret_bitBucket</c>' application setting to the title defined in BitBucket.
+        /// The corresponding WebHook URI is of the form '<c>https://&lt;host&gt;/api/webhooks/incoming/bitbucket</c>'.
+        /// For details about GitHub WebHooks, see <c>https://confluence.atlassian.com/bitbucket/manage-webhooks-735643732.html</c>.
+        /// </summary>
+        /// <param name="config">The current <see cref="HttpConfiguration"/>config.</param>
+        public static void InitializeReceiveGitHubWebHooks(this HttpConfiguration config)
+        {
+            WebHooksConfig.Initialize(config);
+        }
+    }
+}
