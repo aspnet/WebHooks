a7950c9fe7268861fff3939ba689428a99b038c8
Maarten Balliauw <maarten@maartenballiauw.be>
2015-10-02 11:58:44 +0200

    Implement support for MyGet WebHooks
    Add support for MyGet WebHooks - see
    http://docs.myget.org/docs/reference/webhooks

---
+++ src/Microsoft.AspNet.WebHooks.Receivers.MyGet/Payloads/Package.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNet.WebHooks.Payloads
+{
+    /// <summary>
+    /// Package.
+    /// </summary>
+    public class Package
+    {
+        /// <summary>
+        /// Type of the package.
+        /// </summary>
+        public string PackageType { get; set; }
+
+        /// <summary>
+        /// Package identifier.
+        /// </summary>
+        public string PackageIdentifier { get; set; }
+
+        /// <summary>
+        /// Package version.
+        /// </summary>
+        public string PackageVersion { get; set; }
+
+        /// <summary>
+        /// Target framework, if applicable.
+        /// </summary>
+        public string TargetFramework { get; set; }
+    }
+}
---
+++ src/Microsoft.AspNet.WebHooks.Receivers.MyGet/Payloads/PackagePinnedPayload.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.AspNet.WebHooks.Payloads
+{
+    /// <summary>
+    /// Payload sent when a package is pinned/unpinned.
+    /// </summary>
+    public class PackagePinnedPayload
+    {
+        /// <summary>
+        /// Action performed. Will contain "pinned" or "unpinned".
+        /// </summary>
+        public string Action { get; set; }
+
+        /// <summary>
+        /// Package type.
+        /// </summary>
+        public string PackageType { get; set; }
+
+        /// <summary>
+        ///  Package identifier.
+        /// </summary>
+        public string PackageIdentifier { get; set; }
+
+        /// <summary>
+        /// Package version.
+        /// </summary>
+        public string PackageVersion { get; set; }
+
+        /// <summary>
+        /// Containing feed.
+        /// </summary>
+        public string FeedIdentifier { get; set; }
+
+        /// <summary>
+        /// Containing feed URL.
+        /// </summary>
+        public Uri FeedUrl { get; set; }
+    }
+}
