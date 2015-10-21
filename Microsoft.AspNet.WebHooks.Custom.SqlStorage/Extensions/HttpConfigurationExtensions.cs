67f41ec3857272e1b5140adc20e28a15875dcda9
rposener <ryanposener@msn.com>
2015-10-20 22:58:16 -0600

    Added WebHooks SqlStorage Project

---
+++ Microsoft.AspNet.WebHooks.Custom.SqlStorage/Extensions/HttpConfigurationExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNet.DataProtection;
+using Microsoft.AspNet.WebHooks;
+using Microsoft.AspNet.WebHooks.Config;
+using Microsoft.AspNet.WebHooks.Diagnostics;
+using Microsoft.AspNet.WebHooks.Services;
+using Microsoft.Framework.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace System.Web.Http
+{
+    public static class HttpConfigurationExtensions
+    {
+        private const string ApplicationName = "PE.WebHooks";
+        private const string Purpose = "WebHookPersistence";
+        private const string DataProtectionKeysFolderName = "DataProtection-Keys";
+
+        /// <summary>
+        /// Configures a Microsoft Sql Server Storage implementation of <see cref="IWebHookStore"/>
+        /// which provides a persistent store for registered WebHooks used by the custom WebHooks module.
+        /// </summary>
+        /// <param name="config">The current <see cref="HttpConfiguration"/>config.</param>
+        public static void InitializeCustomWebHooksSqlStorage(this HttpConfiguration config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException("config");
+            }
+
+            WebHooksConfig.Initialize(config);
+
+            ILogger logger = config.DependencyResolver.GetLogger();
+            SettingsDictionary settings = config.DependencyResolver.GetSettings();
+
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<WebHookContext, Configuration>());
+
+            IDataProtectionProvider provider = GetDataProtectionProvider();
+            IDataProtector protector = provider.CreateProtector(Purpose);
+
+            IWebHookStore store = new SqlWebHookStore(settings, protector, logger);
+            CustomServices.SetStore(store);
+        }
+
+        /// <summary>
+        /// This follows the same initialization that is provided when <see cref="IDataProtectionProvider"/>
+        /// is initialized within ASP.NET 5.0 Dependency Injection.
+        /// </summary>
+        /// <returns>A fully initialized <see cref="IDataProtectionProvider"/>.</returns>
+        internal static IDataProtectionProvider GetDataProtectionProvider()
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddDataProtection();
+            IServiceProvider services = serviceCollection.BuildServiceProvider();
+            return services.GetDataProtectionProvider();
+        }
+    }
+}
---
+++ Microsoft.AspNet.WebHooks.Custom.SqlStorage/Storage/Registration.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Microsoft.AspNet.WebHooks.Storage
+{
+    [Table("WebHooks")]
+    public class Registration
+    {
+        [Key]
+        [StringLength(256)]
+        [Column(Order =0)]
+        public string User { get; set; }
+
+        [Key]
+        [StringLength(256)]
+        [Column(Order = 1)]
+        public string Id { get; set; }
+
+        [Required]
+        public string ProtectedData { get; set; }
+
+        [Timestamp]
+        public byte[] RowVer { get; set; }
+    }
+}
