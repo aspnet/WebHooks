e4c6d56c52d906bc5de06f09edf5a391b37b5d36
Gaurav Arora <g_arora@hotmail.com>
2015-11-07 18:23:33 +0530

    Added support for Zendesk

---
+++ src/Microsoft.AspNet.WebHooks.Receivers.Zendesk/WebHooks/ZendeskNotification.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Newtonsoft.Json;
+
+namespace Microsoft.AspNet.WebHooks
+{
+    /// <summary>
+    /// Contains the information about notifications
+    /// </summary>
+    public class ZendeskNotification
+    {
+        /// <summary>
+        /// The short message of the notification
+        /// </summary>
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The long message of the notification
+        /// </summary>
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        /// <summary>
+        /// The identifier of the ticket that was updated. Pass this along as zendesk_sdk_request_id if you want ticket deep-linking in the app
+        /// </summary>
+        [JsonProperty("ticket_id")]
+        public string TicketId { get; set; }
+    }
+}
