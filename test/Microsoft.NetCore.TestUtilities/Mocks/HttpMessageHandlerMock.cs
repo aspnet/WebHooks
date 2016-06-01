1cacef8521e5a6fb3695ad2b94373b438a365746
rposener <ryanposener@msn.com>
2016-06-01 00:06:11 -0600

    Common Test Utilities which could be easily converted

---
+++ test/Microsoft.NetCore.TestUtilities/Mocks/HttpMessageHandlerMock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.NetCore.TestUtilities.Mocks
+{
+    /// <summary>
+    /// Provides an <see cref="HttpMessageHandler"/> where the response can be modified in flight by providing
+    /// an appropriate <see cref="Handler"/>.
+    /// </summary>
+    public class HttpMessageHandlerMock : HttpMessageHandler
+    {
+        /// <summary>
+        /// Gets or sets the request counter.
+        /// </summary>
+        public int Counter { get; set; }
+
+        /// <summary>
+        /// The handler which will process the <see cref="HttpRequestMessage"/> and return an <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        public Func<HttpRequestMessage, int, Task<HttpResponseMessage>> Handler { get; set; }
+
+        /// <inheritdoc />
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Handler(request, Counter++);
+        }
+    }
+}
---
+++ test/Microsoft.NetCore.TestUtilities/PropertySetter.cs
@@ -0,0 +1,29 @@
+﻿namespace Microsoft.NetCore.TestUtilities
+{
+    /// <summary>
+    /// Determines how the property under test is expected to behave.
+    /// </summary>
+    public enum PropertySetter
+    {
+        /// <summary>
+        /// It is not allowed to set the property to null
+        /// </summary>
+        NullThrows = 0,
+
+        /// <summary>
+        /// Setting the property to null causes it to get reinitialized to some non-null value
+        /// </summary>
+        NullSetsDefault,
+
+        /// <summary>
+        /// Null round-trips like any other value
+        /// </summary>
+        NullRoundtrips,
+
+        /// <summary>
+        /// Null does not throw and does not round-trip but sets a non-deterministic non-null value.
+        /// This can for example be a GUID or a timestamp.
+        /// </summary>
+        NullDoesNotRoundtrip
+    }
+}
