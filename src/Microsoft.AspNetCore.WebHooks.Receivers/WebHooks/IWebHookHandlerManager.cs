f49680a01698d47aa93dfdfcaebd1150ad329a4b
Doug Bunting <dougbu@microsoft.com>
2017-08-18 16:28:09 -0700

    Add `WebHookHandlerManager`, `SerializableErrorKeys` and more

---
+++ src/Microsoft.AspNetCore.WebHooks.Receivers/WebHooks/IWebHookHandlerManager.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.AspNetCore.WebHooks
+{
+    /// <summary>
+    /// Provides an abstraction for managing <see cref="IWebHookHandler"/> instances which handle incoming WebHook requests.
+    /// </summary>
+    public interface IWebHookHandlerManager
+    {
+        /// <summary>
+        /// Gets the list of available <see cref="IWebHookHandler"/> instances.
+        /// </summary>
+        IReadOnlyList<IWebHookHandler> Handlers { get; }
+    }
+}
---
+++ src/Microsoft.AspNetCore.WebHooks.Receivers/WebHooks/WebHookHandlerManager.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.AspNetCore.WebHooks
+{
+    /// <summary>
+    /// Manages registered <see cref="IWebHookHandler"/> instances.
+    /// </summary>
+    public class WebHookHandlerManager : IWebHookHandlerManager
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebHookHandlerManager"/> class with the given
+        /// <paramref name="handlers"/>, <paramref name="sorter"/> and <paramref name="loggerFactory"/>.
+        /// </summary>
+        public WebHookHandlerManager(
+            IEnumerable<IWebHookHandler> handlers,
+            IWebHookHandlerSorter sorter,
+            ILoggerFactory loggerFactory)
+        {
+            var sortedHandlers = sorter.SortHandlers(handlers);
+            Handlers = new List<IWebHookHandler>(sortedHandlers);
+
+            var logger = loggerFactory.CreateLogger<WebHookHandlerManager>();
+            var receiverNames = string.Join(", ", Handlers.Select(h => h.Receiver));
+            logger.LogInformation(
+                0,
+                "Registered '{Type}' instances for the following receivers: {ReceiverNames}.",
+                typeof(IWebHookHandler).Name,
+                receiverNames);
+        }
+
+        /// <inheritdoc />
+        public IReadOnlyList<IWebHookHandler> Handlers { get; }
+    }
+}
