72ec7b8e3f05c093a2f75af5ad52463c0c11c219
Doug Bunting <dougbu@microsoft.com>
2017-08-19 14:23:29 -0700

    Restore `TypeUtilities`

--- src/Microsoft.AspNetCore.WebHooks.Receivers/Features/WebHookHandlerFeatureProvider.cs
+++ src/Microsoft.AspNetCore.WebHooks.Receivers/Features/WebHookHandlerFeatureProvider.cs
@@ -4,13 +4,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using Microsoft.AspNetCore.WebHooks.Utilities;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 
 namespace Microsoft.AspNetCore.WebHooks.Receivers.Features
 {
+    // Modeled after ControllerFeatureProvider in ASP.NET Core MVC.
     /// <summary>
-    /// Discovers controllers from a list of <see cref="ApplicationPart"/> instances.
+    /// Discovers handlers from a list of <see cref="ApplicationPart"/> instances.
     /// </summary>
     public class WebHookHandlerFeatureProvider : IApplicationFeatureProvider<WebHookHandlerFeature>
     {
@@ -30,45 +31,12 @@ namespace Microsoft.AspNetCore.WebHooks.Receivers.Features
             {
                 foreach (var type in part.Types)
                 {
-                    if (IsHandler(type) && !feature.Handlers.Contains(type))
+                    if (TypeUtilities.IsType<IWebHookHandler>(type) && !feature.Handlers.Contains(type))
                     {
                         feature.Handlers.Add(type);
                     }
                 }
             }
-        }
-
-        /// <summary>
-        /// Determines if a given <paramref name="typeInfo"/> is a handler.
-        /// </summary>
-        /// <param name="typeInfo">The <see cref="TypeInfo"/> candidate.</param>
-        /// <returns><code>true</code> if the type is a handler; otherwise <code>false</code>.</returns>
-        protected virtual bool IsHandler(TypeInfo typeInfo)
-        {
-            if (!typeInfo.IsClass)
-            {
-                return false;
-            }
-
-            if (typeInfo.IsAbstract)
-            {
-                return false;
-            }
-
-            // ??? Okay that next two checks are a bit more stringent than TypeUtilities.IsType<T>()'s IsVisible check?
-            // We only consider public top-level classes as receivers. IsPublic returns false for nested
-            // classes, regardless of visibility modifiers
-            if (!typeInfo.IsPublic)
-            {
-                return false;
-            }
-
-            if (typeInfo.ContainsGenericParameters)
-            {
-                return false;
-            }
-
-            return typeof(IWebHookHandler).GetTypeInfo().IsAssignableFrom(typeInfo);
         }
     }
 }
--- src/Microsoft.AspNetCore.WebHooks.Receivers/Features/WebHookReceiverFeatureProvider.cs
+++ src/Microsoft.AspNetCore.WebHooks.Receivers/Features/WebHookReceiverFeatureProvider.cs
@@ -4,13 +4,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using Microsoft.AspNetCore.WebHooks.Utilities;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 
 namespace Microsoft.AspNetCore.WebHooks.Receivers.Features
 {
+    // Modeled after ControllerFeatureProvider in ASP.NET Core MVC.
     /// <summary>
-    /// Discovers controllers from a list of <see cref="ApplicationPart"/> instances.
+    /// Discovers receivers from a list of <see cref="ApplicationPart"/> instances.
     /// </summary>
     public class WebHookReceiverFeatureProvider : IApplicationFeatureProvider<WebHookReceiverFeature>
     {
@@ -30,45 +31,12 @@ namespace Microsoft.AspNetCore.WebHooks.Receivers.Features
             {
                 foreach (var type in part.Types)
                 {
-                    if (IsReceiver(type) && !feature.Receivers.Contains(type))
+                    if (TypeUtilities.IsType<IWebHookReceiver>(type) && !feature.Receivers.Contains(type))
                     {
                         feature.Receivers.Add(type);
                     }
                 }
             }
-        }
-
-        /// <summary>
-        /// Determines if a given <paramref name="typeInfo"/> is a receiver.
-        /// </summary>
-        /// <param name="typeInfo">The <see cref="TypeInfo"/> candidate.</param>
-        /// <returns><code>true</code> if the type is a receiver; otherwise <code>false</code>.</returns>
-        protected virtual bool IsReceiver(TypeInfo typeInfo)
-        {
-            if (!typeInfo.IsClass)
-            {
-                return false;
-            }
-
-            if (typeInfo.IsAbstract)
-            {
-                return false;
-            }
-
-            // ??? Okay that next two checks are a bit more stringent than TypeUtilities.IsType<T>()'s IsVisible check?
-            // We only consider public top-level classes as receivers. IsPublic returns false for nested
-            // classes, regardless of visibility modifiers
-            if (!typeInfo.IsPublic)
-            {
-                return false;
-            }
-
-            if (typeInfo.ContainsGenericParameters)
-            {
-                return false;
-            }
-
-            return typeof(IWebHookReceiver).GetTypeInfo().IsAssignableFrom(typeInfo);
         }
     }
 }
