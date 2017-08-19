18fd418875c00139d820deb0475deada52bf8dc3
Doug Bunting <dougbu@microsoft.com>
2017-08-19 14:30:20 -0700

    Add `WebHookApplicationBuilderExtensions` (syntactic sugar)

---
+++ src/Microsoft.AspNetCore.WebHooks.Receivers/Extensions/WebHookApplicationBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace Microsoft.AspNetCore.WebHooks.Extensions
+{
+    public static class WebHookApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseWebHooks(this IApplicationBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            // Handle index.html files that user projects often include.
+            builder.UseStaticFiles();
+            builder.UseDefaultFiles();
+
+            // Few MVC requirements beyond attribute routing.
+            builder.UseMvc();
+
+            return builder;
+        }
+    }
+}
