dc1c66ea38bffcc7ab1ad4606f1f5a1fab43a833
Doug Bunting <dougbu@microsoft.com>
2017-09-14 11:20:00 -0700

    Add initial GitHub example for ASP.NET Core

---
+++ samples/GitHubCoreReceiver/Controllers/GitHubController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json.Linq;
+
+namespace GitHubCoreReceiver.Controllers
+{
+    public class GitHubController : ControllerBase
+    {
+        [GitHubWebHookAction(EventName = "push")]
+        public IActionResult HandlerForPush(string id, JObject data)
+        {
+            return Ok();
+        }
+
+        [GitHubWebHookAction(Id = "It")]
+        public IActionResult HandlerForIt(string[] events, JObject data)
+        {
+            return Ok();
+        }
+
+        [GitHubWebHookAction]
+        public IActionResult GitHubHandler(string id, string @event, JObject data)
+        {
+            return Ok();
+        }
+
+        [WebHookAction]
+        public IActionResult FallbackHandler(string receiverName, string id, string eventName, JObject data)
+        {
+            return Ok();
+        }
+    }
+}
