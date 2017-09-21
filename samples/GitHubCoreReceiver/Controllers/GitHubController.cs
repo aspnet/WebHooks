87a58f52bd82f1ba40a8afd9ff36dfa2d028e810
Doug Bunting <dougbu@microsoft.com>
2017-09-21 08:01:52 -0700

    Add a few more samples: Azure Alert, Bitbucket (x2) and Kudu - also add a bit to `GitHubController` in the GitHub sample
    `BitbucketStronglyTypedCoreReceiver` sample
    - shows necessary classes for a single Bitbucket notification type if not using `JObject`
    - leaves out at least a few `IDictionary<string, BitbucketLink> Links` and `bool` properties

--- samples/GitHubCoreReceiver/Controllers/GitHubController.cs
+++ samples/GitHubCoreReceiver/Controllers/GitHubController.cs
@@ -6,27 +6,58 @@ namespace GitHubCoreReceiver.Controllers
 {
     public class GitHubController : ControllerBase
     {
-        [GitHubWebHook(EventName = "push")]
-        public IActionResult HandlerForPush(string id, JObject data)
+        [GitHubWebHook(EventName = "push", Id = "It")]
+        public IActionResult HandlerForItsPushes(string[] events, JObject data)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             return Ok();
         }
 
         [GitHubWebHook(Id = "It")]
         public IActionResult HandlerForIt(string[] events, JObject data)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [GitHubWebHook(EventName = "push")]
+        public IActionResult HandlerForPush(string id, JObject data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             return Ok();
         }
 
         [GitHubWebHook]
         public IActionResult GitHubHandler(string id, string @event, JObject data)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             return Ok();
         }
 
         [GeneralWebHook]
         public IActionResult FallbackHandler(string receiverName, string id, string eventName, JObject data)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             return Ok();
         }
     }
---
+++ samples/KuduCoreReceiver/Controllers/KuduController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+
+namespace AzureAlertCoreReceiver.Controllers
+{
+    public class KuduController : ControllerBase
+    {
+        [KuduWebHook(Id = "It")]
+        public IActionResult AzureAlertForIt(KuduNotification data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Get the notification message
+            var message = data.Message;
+
+            // Get the notification author
+            var author = data.Author;
+
+            return Ok();
+        }
+
+        [KuduWebHook]
+        public IActionResult AzureAlert(string id, KuduNotification data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Get the notification message
+            var message = data.Message;
+
+            // Get the notification author
+            var author = data.Author;
+
+            return Ok();
+        }
+    }
+}
