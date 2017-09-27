5384173c093d14d18fe52a0f3292017e5bb0a78d
Doug Bunting <dougbu@microsoft.com>
2017-09-27 14:10:34 -0700

    Add rest of Pusher receiver

---
+++ src/Microsoft.AspNetCore.WebHooks.Receivers.Pusher/Extensions/PusherMvcCoreBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel;
+using Microsoft.AspNetCore.WebHooks.Filters;
+using Microsoft.AspNetCore.WebHooks.Metadata;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    // TODO: Add IMvcBuilder variant of this class.
+    /// <summary>
+    /// Extension methods for setting up Pusher WebHooks in an <see cref="IMvcCoreBuilder" />.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class PusherMvcCoreBuilderExtensions
+    {
+        /// <summary>
+        /// Add Pusher WebHook configuration and services to the specified <paramref name="builder"/>.
+        /// </summary>
+        /// <param name="builder">The <see cref="IMvcCoreBuilder" /> to configure.</param>
+        public static IMvcCoreBuilder AddPusherWebHooks(this IMvcCoreBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IWebHookMetadata, PusherMetadata>());
+
+            return builder
+                .AddJsonFormatters()
+                .AddWebHooks()
+                .AddSingletonFilter<PusherVerifySignatureFilter>(WebHookSecurityFilter.Order);
+        }
+    }
+}
